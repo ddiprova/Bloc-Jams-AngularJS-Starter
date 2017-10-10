1552c7c9caaa2c2478d2da96dce34bf2271787fe
Don DiProva <ddiprova@aol.com>
2017-10-09 20:54:56 -0700

    chapter-7-services-pt2-fix committed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,34 +1,30 @@
 (function() {
      function SongPlayer() {
           var SongPlayer = {};
-
           var currentSong = null;
-
 /**
 * @desc Buzz object audio file
 * @type {Object}
 */
           var currentBuzzObject = null;
-
 /**
 * @function setSong
 * @desc Stops currently playing song and loads new audio file as currentBuzzObject
 * @param {Object} song
 */
-
     var setSong = function(song) {
       if (currentBuzzObject) {
-       currentBuzzObject.stop();
-       currentSong.playing = null;
+        currentBuzzObject.stop();
+        currentSong.playing = null;
    }
-
-   currentBuzzObject = new buzz.sound(song.audioUrl, {
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
     });
-       currentSong = song;
-
-     };
+        currentSong = song;
+    };
+        return SongPlayer;
+};
 
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
@@ -40,20 +36,12 @@
                 currentBuzzObject.play();
             }
         }
+      };
 
-         currentBuzzObject = new buzz.sound(song.audioUrl, {
-             formats: ['mp3'],
-             preload: true
-         });
-       };
-
-     SongPlayer.pause = function(song) {
-       currentBuzzObject.pause();
-       song.playing = false;
+       SongPlayer.pause = function(song) {
+          currentBuzzObject.pause();
+          song.playing = false;
      };
-
-      return SongPlayer;
-    };
 
      angular
          .module('blocJams')
