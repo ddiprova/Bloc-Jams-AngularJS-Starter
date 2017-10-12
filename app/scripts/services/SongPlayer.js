15c13c392841b2656ee3e2f86a8acc37b9f2b77d
Don DiProva <ddiprova@aol.com>
2017-10-11 18:54:09 -0700

    chapter-8-services-pt2 committed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,17 +1,19 @@
 (function() {
-     function SongPlayer() {
+      function SongPlayer(Fixtures) {
           var SongPlayer = {};
 
           /**
-          * @desc current song placeholder
+          * @desc access the album
           * @type {Object}
           */
-          var currentSong = null;
+          var currentAlbum = Fixtures.getAlbum();
+
           /**
           * @desc Buzz object audio file
           * @type {Object}
           */
           var currentBuzzObject = null;
+
           /**
           * @function setSong
           * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -20,14 +22,14 @@
           var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
               }
 
               currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
               });
-              currentSong = song;
+              SongPlayer.currentSong = song;
             };
 
           /*
@@ -40,32 +42,65 @@
             song.playing = true;
           };
 
-    /* @function SongPlayer.play
-    * @desc Checks current song, if not equal to selected song, sets song to current playing song, begins playing song
-    *       if it is selected song, pause the current song and play the selected song
+          /**
+          * @desc Get song index from album
+          * @type {Object}
+          */
+          var getSongIndex = function(song) {
+              return currentAlbum.songs.indexOf(song);
+            };
+
+          /**
+          * @desc Active song object from list of songs
+          * @type {Object}
+          */
+          SongPlayer.currentSong = null;
+
+    /* @function play
+    * @desc Play current or new song
     * @param {Object} song
     */
 
     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) {
           setSong(song);
           playSong(song);
-      } else if (currentSong === song) {
+      } else if (SongPlayer.currentSong === song) {
             if (currentBuzzObject.isPaused()) {
                 playSong(song);
             }
         }
       };
 
-      /* @function SongPlayer.pause
+      /* @function pause
       * @desc Pauses the current playing song
       * @param {Object} song
       */
 
        SongPlayer.pause = function(song) {
+          song = song || SongPlayer.currentSong;
           currentBuzzObject.pause();
           song.playing = false;
      };
+
+     /* @function previous song
+     * @desc get the previous song on the album
+     * @param {Object} SongPlayer.currentSong
+     */
+     SongPlayer.previous = function() {
+     var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+     currentSongIndex--;
+
+     if (currentSongIndex < 0) {
+         currentBuzzObject.stop();
+         SongPlayer.currentSong.playing = null;
+       } else {
+          var song = currentAlbum.songs[currentSongIndex];
+          setSong(song);
+          playSong(song);
+      }
+ };
         return SongPlayer;
     };
 
@@ -73,5 +108,5 @@
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['Fixtures', SongPlayer]);
  })();
